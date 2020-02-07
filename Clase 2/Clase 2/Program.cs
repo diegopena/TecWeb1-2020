bffb193a3a7148ae885d05933f6962c148f672d9
Diego Penaranda <diego.npa.1998@gmail.com>
2020-02-06 20:31:54 -0400

    added classes

---
+++ Clase 2/Clase 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Clase_2
+{
+    public class Shape
+    {
+        public string name { get; set; }
+
+        public string color { get; set; }
+
+        public virtual void Show()
+        {
+            //Console.WriteLine(string.Format("my name is {0} shape", name));
+            Console.WriteLine($"my name ins {name} shape");
+        }
+    }
+
+    public class Square : Shape
+    {
+        public override void Show()
+        {
+            Console.WriteLine($"im a square {name}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Shape myShape = new Shape("basic");
+            Shape myShape = new Square() {name = "basic", color = "blue"};
+            myShape.Show();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
